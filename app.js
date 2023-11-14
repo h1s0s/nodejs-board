645f08176350f96250b3b020d76ff062eac78602
한상선 <hs94122@naver.com>
2023-11-14 14:09:44 +0900

    미들웨어 추가 등

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const mysql = require('mysql2');
-const path = require('path');
 const dbconfig = require('./config/database.js');
+const path = require('path');
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
@@ -32,14 +37,48 @@ app.use((req, res, next) => {
 });
 
 // 라우터 정의
-const globalRouter = require('./src/routers/globalRouter.js');
-// const userRouter = require('./routers/userRouter');
-const boardRouter = require('./src/routers/boardRouter.js');
+const indexRouter = require('./src/routes');
+const userRouter = require('./src/routes/user');
+const boardRouter = require('./src/routes/board');
 
-// 라우터를 Express 애플리케이션에 등록
-app.use('/', globalRouter);
-// app.use('/users', userRouter);
-app.use('/board', boardRouter);
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie'
+}));
+
+// 라우터를 
+app.use('/', indexRouter);
+// app.use('/user', userRouter);
+// app.use('/board', boardRouter);
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+})
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에 대해서만 실행됩니다.');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+});
+app.use((req, res, next) => {
+    res.status(404).send("Not Found");
+});
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
 
 // 서버 시작
 app.listen(port, () => {
--- src/controller/boardController.js
+++ src/controller/boardController.js
@@ -1,71 +1,71 @@
-const boardModel = require('../model/boardModel');
+// const boardModel = require('../model/board');
 
-/**
- * @function renderBoard
- * @description 게시판 페이지 렌더링
- * @param {object} req
- * @param {object} res
- */
-function renderBoard(req, res) {
-    boardModel.getAllBoards((err, results, fields) => {
-        if (err) {
-            console.error('Error getting boards:', err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
+// /**
+//  * @function renderBoard
+//  * @description 게시판 페이지 렌더링
+//  * @param {object} req
+//  * @param {object} res
+//  */
+// function renderBoard(req, res) {
+//     boardModel.getAllBoards((err, results, fields) => {
+//         if (err) {
+//             console.error('Error getting boards:', err);
+//             res.status(500).send('Internal Server Error');
+//             return;
+//         }
 
-        res.render('board/board', { results });
-    });
-}
+//         res.render('board/board', { results });
+//     });
+// }
 
-/**
- * @function createBoard
- * @description 게시판 페이지 렌더링
- * @param {object} req
- * @param {object} res
- */
-const createBoard = async (req, res) => {
-    const { writer, content, password } = req.body;
+// /**
+//  * @function createBoard
+//  * @description 게시판 페이지 렌더링
+//  * @param {object} req
+//  * @param {object} res
+//  */
+// const createBoard = async (req, res) => {
+//     const { writer, content, password } = req.body;
 
-    try {
-        // TODO: 실제 데이터베이스에 게시글 추가 로직을 구현
-        const postId = await addPostToDatabase(writer, content, password);
+//     try {
+//         // TODO: 실제 데이터베이스에 게시글 추가 로직을 구현
+//         const postId = await addPostToDatabase(writer, content, password);
 
-        // 추가된 게시글 정보를 응답
-        const newPost = { postId, writer, content };
-        res.json(newPost);
-    } catch (error) {
-        console.error('Error creating post:', error);
-        res.status(500).send('Internal Server Error');
-    }
-};
+//         // 추가된 게시글 정보를 응답
+//         const newPost = { postId, writer, content };
+//         res.json(newPost);
+//     } catch (error) {
+//         console.error('Error creating post:', error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// };
 
-/**
- * @function deleteBoard
- * @description 게시판 페이지 렌더링
- * @param {object} req
- * @param {object} res
- */
-const deleteBoard = async (req, res) => {
-    const postId = req.params.postId;
-    const password = req.body.password;
+// /**
+//  * @function deleteBoard
+//  * @description 게시판 페이지 렌더링
+//  * @param {object} req
+//  * @param {object} res
+//  */
+// const deleteBoard = async (req, res) => {
+//     const postId = req.params.postId;
+//     const password = req.body.password;
 
-    try {
-        // TODO: 실제 데이터베이스에서 게시글 삭제 로직을 구현
-        const success = await deletePostFromDatabase(postId, password);
+//     try {
+//         // TODO: 실제 데이터베이스에서 게시글 삭제 로직을 구현
+//         const success = await deletePostFromDatabase(postId, password);
 
-        if (success) {
-            res.json({ message: 'Post deleted successfully' });
-        } else {
-            res.status(404).json({ message: 'Post not found or password incorrect' });
-        }
-    } catch (error) {
-        console.error('Error deleting post:', error);
-        res.status(500).send('Internal Server Error');
-    }
-};
-module.exports = {
-    renderBoard,
-    createBoard,
-    deleteBoard
-};
+//         if (success) {
+//             res.json({ message: 'Post deleted successfully' });
+//         } else {
+//             res.status(404).json({ message: 'Post not found or password incorrect' });
+//         }
+//     } catch (error) {
+//         console.error('Error deleting post:', error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// };
+// module.exports = {
+//     renderBoard,
+//     createBoard,
+//     deleteBoard
+// };
